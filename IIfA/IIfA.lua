bd559fbd5ff16f80e101cdeddb60daac747617e9
AssemblerManiac <amaniac-github@shredco.com>
2018-03-06 11:47:36 -0500

    settings adapter line 60 bad control ref fixed

--- IIfA/IIfA.lua
+++ IIfA/IIfA.lua
@@ -21,7 +21,7 @@ if IIfA == nil then IIfA = {} end
 --local IIfA = IIfA
 
 IIfA.name 				= "Inventory Insight"
-IIfA.version 			= "3.13"
+IIfA.version 			= "3.13a"
 IIfA.author 			= "AssemblerManiac & manavortex"
 IIfA.defaultAlertSound 	= nil
 IIfA.colorHandler 		= nil
