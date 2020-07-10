7722c7b787247647f000427a40a697caf20975ca
Baertram <pz1@gmx.net>
2020-07-09 18:09:59 -0700

    Updated: Mod Version

--- IIfA/IIfA.lua
+++ IIfA/IIfA.lua
@@ -15,7 +15,7 @@ DISCLAIMER
 if IIfA == nil then IIfA = {} end
 
 IIfA.name 				= "Inventory Insight"
-IIfA.version 			= "3.40"
+IIfA.version 			= "3.45"
 IIfA.author 			= "AssemblerManiac & manavortex"
 IIfA.defaultAlertSound 	= nil
 IIfA.colorHandler 		= nil
